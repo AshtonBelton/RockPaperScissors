26bd31b6b19253f7b3e8542048f8af50c7e5a3bd
Ashton Belton <ashbelton24@gmail.com>
2024-08-08 19:59:17 -0600

    Done html and css for now. Working on JavaScript now

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const battlefield = document.querySelector(".battlefield");
+
+const playerBattle = document.querySelector(".player-battle");
+const player = document.querySelector(".player")
+
+const computerBattle = document.querySelector(".computer-battle");
+const computer = document.querySelector(".computer");
+
+const playerChoices = document.querySelector(".player-choices");
+const buttonChoices = document.querySelector(".buttons");
+
