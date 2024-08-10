9c30688b4ed5710865db097be1c0d751c0d25e54
Ashton Belton <ashbelton24@gmail.com>
2024-08-10 17:31:38 -0600

    Making the game run smoother and look better

--- main.js
+++ main.js
@@ -18,8 +18,9 @@ const final = document.querySelector(".final");
 
 /* Game logic */
 
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+const maxScore = 5;
 
 function getComputerChoice() {
     const value = ["mario", "peach", "bowser"];
@@ -45,16 +46,31 @@ function playRound(humanChoice, computerChoice) {
 
     if (winCoditions[humanChoice] === computerChoice) {
         humanScore++;
-        title.textContent = "You won!";
+        title.textContent = "You won this round!";
         playerPoint.textContent = `Player: ${humanScore}`;
     } else {
         computerScore++;
-        title.textContent = "You lose!";
+        title.textContent = "You lost this round!";
         computerPoint.textContent = `Computer: ${computerScore}`;
+    }
+
+    if (humanScore === maxScore || computerScore === maxScore) {
+        declareFinalWinner();
     }
 }
 
-function playGame() {
+function declareFinalWinner() {
+    characters.style.pointerEvents = "none";
+    if (humanScore > computerScore) {
+        final.textContent = "Congratulations! You win the game!";
+    } else if (computerScore > humanScore) {
+        final.textContent = "Game Over! The computer wins the game!";
+    } else {
+        final.textContent = "It's a draw! No one wins the game!";
+    }
+}
+
+/*function playGame() {
     humanScore = 0;
     computerScore = 0;
 
@@ -68,13 +84,13 @@ function playGame() {
     finalResult.textContent = `Final Score - Human: ${humanScore}, Computer: ${computerScore}`;
     
     if (humanScore > computerScore) {
-        final.textContent = "You win the game!";
+        final.textContent = "You won the game!";
     } else if (computerScore > humanScore) {
-        final.textContent = "You lose the game!";
+        final.textContent = "You lost the game!";
     } else {
         final.textContent = "Draw! No one wins!";
     }
-}
+}*/
 
 characters.onclick = (event) => {
     const buttonClicked = event.target.getAttribute("id");
@@ -82,19 +98,33 @@ characters.onclick = (event) => {
     const computerCharacter = getComputerChoice();
 
     const playerImgChoice = document.createElement("img");
-    playerImgChoice.src = `./images/${buttonClicked}.png`;
-    playerImgChoice.setAttribute("class", "character-choice");
+    playerImgChoice.src = `./images/${humanCharacter}.png`;
+    playerImgChoice.setAttribute("class", `character-choice ${humanCharacter}`);
 
     const computerImgChoice = document.createElement("img");
-    playerImgChoice.src = `./images/${computerCharacter}.png`;
-    playerImgChoice.setAttribute("class", "character-choice");
+    computerImgChoice.src = `./images/${computerCharacter}.png`;
+    computerImgChoice.setAttribute("class", `character-choice ${computerCharacter}`);
 
+    /*
     if (blockStagePlayer.childElementCount == 1) {
         blockStagePlayer.insertBefore(playerImgChoice, blockStagePlayer.firstchild);
         blockStageComputer.insertBefore(computerImgChoice, blockStageComputer.firstChild);
     } else {
         blockStagePlayer.replaceChild(playerImgChoice, blockStagePlayer.firstChild);
         blockStageComputer.replaceChild(computerImgChoice, blockStageComputer.firstChild);
+    }
+    */
+
+    if (blockStagePlayer.childElementCount == 1) {
+        blockStagePlayer.appendChild(playerImgChoice);
+    } else {
+        blockStagePlayer.replaceChild(playerImgChoice, blockStagePlayer.lastChild);
+    }
+
+    if (blockStageComputer.childElementCount == 1) {
+        blockStageComputer.appendChild(computerImgChoice);
+    } else {
+        blockStageComputer.replaceChild(computerImgChoice, blockStageComputer.lastChild);
     }
 
     playRound(humanCharacter, computerCharacter);
